35adcd9e452b8346ba16735baa73183cd9248b08
Otsu.Hiroaki <otsu.hiroaki@moneyforward.co.jp>
2017-07-09 05:09:07 +0900

    update

--- action/ag.go
+++ action/ag.go
@@ -4,7 +4,7 @@ import (
 	"flag"
 	"os/exec"
 	"fmt"
-	"regexp"
+	// "regexp"
 )
 
 type ag struct {}
@@ -23,7 +23,8 @@ func (self *ag) Do(args []string) error {
 	out, err := exec.Command("ag", cmd_args...).Output()
 	if err != nil { return err }
 
-	local_root_re, _ := regexp.Compile("(?m)^"+Context.Root())
-	fmt.Print(local_root_re.ReplaceAllString(string(out), ""))
+	// local_root_re, _ := regexp.Compile("(?m)^"+Context.Root())
+	// fmt.Print(local_root_re.ReplaceAllString(string(out), ""))
+	fmt.Print(string(out))
 	return nil
 }
