a484e470a7badc528da68a3ed727830201689cd5
Otsu.Hiroaki <otsu.hiroaki@moneyforward.co.jp>
2017-07-11 23:21:46 +0900

    cat -peco

--- action/cat.go
+++ action/cat.go
@@ -6,12 +6,14 @@ import (
 	"fmt"
 	"encoding/json"
 	"strconv"
+	"io"
 	"github.com/upamune/go-esa/esa"
 	"github.com/aki2o/esa-cui/util"
 )
 
 type cat struct {
 	json_format bool
+	pecolize bool
 }
 
 type postProperty struct {
@@ -26,11 +28,19 @@ func init() {
 
 func (self *cat) SetOption(flagset *flag.FlagSet) {
 	flagset.BoolVar(&self.json_format, "json", false, "Show properties as json.")
+	flagset.BoolVar(&self.pecolize, "peco", false, "Exec with peco.")
 }
 
 func (self *cat) Do(args []string) error {
 	var path string = ""
 	if len(args) > 0 { path = args[0] }
+
+	if self.pecolize {
+		next_path, err := self.runPeco(path)
+		if err != nil { return err }
+
+		path = next_path
+	}
 
 	dir_path, post_number := DirectoryPathAndPostNumberOf(path)
 	if post_number == "" {
@@ -55,4 +65,15 @@ func (self *cat) Do(args []string) error {
 		fmt.Println(string(bytes))
 	}
 	return nil
+}
+
+func (self *cat) runPeco(path string) (string, error) {
+	provider := func(writer *io.PipeWriter) {
+		defer writer.Close()
+		
+		ls := &ls{ writer: writer, recursive: true, file_only: true }
+		ls.printNodesIn(path, AbsolutePathOf(path))
+	}
+
+	return pipePeco(provider)
 }
