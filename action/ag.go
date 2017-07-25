f40d3a9a190a42e9d3599c823d48fbc111b0a939
Otsu.Hiroaki <otsu.hiroaki@moneyforward.co.jp>
2017-07-25 20:58:47 +0900

    fix bug

--- action/ag.go
+++ action/ag.go
@@ -8,6 +8,8 @@ import (
 	"io"
 	"bufio"
 	"errors"
+	"encoding/json"
+	"github.com/aki2o/go-esa/esa"
 )
 
 type ag struct {
@@ -24,9 +26,9 @@ func (self *ag) SetOption(flagset *flag.FlagSet) {
 
 func (self *ag) Do(args []string) error {
 	cmd_args := append([]string{"-G", ".md$"}, args...)
-	cmd_args = append(cmd_args, Context.Cwd)
+	cmd_args = append(cmd_args, Context.BodyRoot())
 
-	local_root_re, _ := regexp.Compile("(?m)^"+Context.Root())
+	path_re, _ := regexp.Compile("(?m)^"+Context.BodyRoot()+"/[0-9]+\\.md:[0-9]+:")
 	
 	var result string
 	var out []byte
@@ -41,7 +43,7 @@ func (self *ag) Do(args []string) error {
 
 			rich_writer := bufio.NewWriter(writer)
 
-			fmt.Fprintf(rich_writer, local_root_re.ReplaceAllString(string(out), ""))
+			fmt.Fprintf(rich_writer, path_re.ReplaceAllStringFunc(string(out), self.appendPostName))
 			rich_writer.Flush()
 		}
 
@@ -50,7 +52,7 @@ func (self *ag) Do(args []string) error {
 		out, err = exec.Command("ag", cmd_args...).Output()
 		if err != nil { return err }
 		
-		result = local_root_re.ReplaceAllString(string(out), "")
+		result = path_re.ReplaceAllStringFunc(string(out), self.appendPostName)
 	}
 	
 	if err != nil { return err }
@@ -58,8 +60,8 @@ func (self *ag) Do(args []string) error {
 	if self.pecolize {
 		if result == "" { return nil }
 		
-		path_re, _ := regexp.Compile("^(.+?)\\.md:[0-9]+:")
-		matches := path_re.FindStringSubmatch(result)
+		re, _ := regexp.Compile("^([0-9]+):[0-9]+:")
+		matches := re.FindStringSubmatch(result)
 
 		if len(matches) > 1 {
 			open := &open{}
@@ -71,4 +73,18 @@ func (self *ag) Do(args []string) error {
 		fmt.Print(result)
 		return nil
 	}
+}
+
+func (self *ag) appendPostName(path string) string {
+	re, _ := regexp.Compile("([0-9]+)\\.md:([0-9]+):$")
+	matches := re.FindStringSubmatch(path)
+	if len(matches) <= 2 { return path }
+
+	bytes, err := LoadPostData(matches[1])
+	if err != nil { return path }
+
+	var post esa.PostResponse
+	if err := json.Unmarshal(bytes, &post); err != nil { return path }
+
+	return fmt.Sprintf("%s:%s:%s: ", matches[1], matches[2], post.FullName)
 }
