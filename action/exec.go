daa8aadd44bab02facaff6222ab95f7d66204b69
Otsu.Hiroaki <otsu.hiroaki@moneyforward.co.jp>
2017-08-30 00:03:11 +0900

    exec --shell

--- action/exec.go
+++ action/exec.go
@@ -2,11 +2,13 @@ package action
 
 import (
 	osexec "os/exec"
+	"strings"
 	"github.com/aki2o/esal/util"
 )
 
 type exec struct {
 	util.ProcessIO
+	ShellCommand string `short:"s" long:"shell" description:"Execute by SHELL." value-name:"SHELL"`
 }
 
 func init() {
@@ -14,10 +16,18 @@ func init() {
 }
 
 func (self *exec) Do(args []string) error {
-	cmd := osexec.Command(args[0], args[1:]...)
+	var cmd *osexec.Cmd
+	
+	if self.ShellCommand != "" {
+		cmd = osexec.Command(self.ShellCommand, "-c", strings.Join(args, " "))
+	} else {
+		cmd = osexec.Command(args[0], args[1:]...)
+	}
+	
 	cmd.Stdout = self.Writer
 	cmd.Stdin  = self.Reader
 	cmd.Start()
 	cmd.Wait()
+	
 	return nil
 }
