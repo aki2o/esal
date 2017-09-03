438284f2673704fca478baec23092676e3431926
Otsu.Hiroaki <otsu.hiroaki@moneyforward.co.jp>
2017-09-03 10:24:26 +0900

    add test

--- action/write_test.go
+++ action/write_test.go
@@ -35,7 +35,7 @@ func TestAppend(t *testing.T) {
 	}
 }
 
-func TestInsert(t *testing.T) {
+func TestInsertSingle(t *testing.T) {
 	write_process := &write{ InsertConditions: []string{ "two" } }
 
 	body := []string{
@@ -53,9 +53,83 @@ func TestInsert(t *testing.T) {
 
 	new_body, err := write_process.makeBody(strings.Join(body, "\r\n"), []string{ write_text })
 	if err != nil {
-		t.Errorf("Failed to insert : %s", err.Error)
+		t.Errorf("Failed to insert single : %s", err.Error)
 	}
 	if new_body != strings.Join(expect_body, "\r\n") {
-		t.Errorf("Failed to insert.\n[expect]\n%s\n[actual]\n%s", strings.Join(expect_body, "\r\n"), new_body)
+		t.Errorf("Failed to insert single.\n[expect]\n%s\n[actual]\n%s", strings.Join(expect_body, "\r\n"), new_body)
+	}
+}
+
+func TestInsertMulti(t *testing.T) {
+	write_process := &write{ InsertConditions: []string{ "two", "dos" } }
+
+	body := []string{
+		"# one",
+		"## uno",
+		"## dos",
+		"## tres",
+		"# two",
+		"## uno",
+		"## dos",
+		"## tres",
+		"# three",
+	}
+	write_text := "Hey!!!"
+	expect_body := []string{
+		"# one",
+		"## uno",
+		"## dos",
+		"## tres",
+		"# two",
+		"## uno",
+		"## dos",
+		"Hey!!!",
+		"## tres",
+		"# three",
+	}
+
+	new_body, err := write_process.makeBody(strings.Join(body, "\r\n"), []string{ write_text })
+	if err != nil {
+		t.Errorf("Failed to insert multi : %s", err.Error)
+	}
+	if new_body != strings.Join(expect_body, "\r\n") {
+		t.Errorf("Failed to insert multi.\n[expect]\n%s\n[actual]\n%s", strings.Join(expect_body, "\r\n"), new_body)
+	}
+}
+
+func TestInsertSingleIntoMultiHead(t *testing.T) {
+	write_process := &write{ InsertConditions: []string{ "two" } }
+
+	body := []string{
+		"# one",
+		"## uno",
+		"## dos",
+		"## tres",
+		"# two",
+		"## uno",
+		"## dos",
+		"## tres",
+		"# three",
+	}
+	write_text := "Hey!!!"
+	expect_body := []string{
+		"# one",
+		"## uno",
+		"## dos",
+		"## tres",
+		"# two",
+		"## uno",
+		"## dos",
+		"## tres",
+		"Hey!!!",
+		"# three",
+	}
+
+	new_body, err := write_process.makeBody(strings.Join(body, "\r\n"), []string{ write_text })
+	if err != nil {
+		t.Errorf("Failed to insert single into multi head : %s", err.Error)
+	}
+	if new_body != strings.Join(expect_body, "\r\n") {
+		t.Errorf("Failed to insert single into multi head.\n[expect]\n%s\n[actual]\n%s", strings.Join(expect_body, "\r\n"), new_body)
 	}
 }
